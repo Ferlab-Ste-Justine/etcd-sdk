99940625f077c9b6de3ae60217194844651d1367
Eric Vallee <eric_vallee@webificservices.com>
2023-05-01 14:51:29 -0400

    feat: INFRA-160 Added request handler for keypb server

--- keypb/keydiff.go
+++ keypb/keydiff.go
@@ -1,8 +1,79 @@
 package keypb
 
 import (
+	"errors"
+
 	"github.com/Ferlab-Ste-Justine/etcd-sdk/client"
 )
+
+var (
+	ErrKeyDiffRequestNotOverview = errors.New("First KeyDiff request was not an overview")
+	ErrKeyDiffRequestNotChanges = errors.New("KeyDiff request after first was not change")
+	ErrKeyDiffRequestOpsMiscount = errors.New("Number of operations in KeyDiff requests did not match overview")
+	ErrInvalidChangeType = errors.New("Change type for one of the changes was not valid")
+)
+
+type KeyDiffResult struct {
+	KeyDiff client.KeyDiff
+	Error error
+}
+
+func GetKeyDiffFromRequests(reqCh <-chan *SendKeyDiffRequest) <-chan KeyDiffResult {
+	inserts := int64(0)
+	updates := int64(0)
+	deletions := int64(0)
+	diff := client.KeyDiff{
+		Inserts: map[string]string{},
+		Updates: map[string]string{},
+		Deletions: []string{},
+	}
+	resCh := make(chan KeyDiffResult)
+
+	go func() {
+		defer close(resCh)
+		req := <- reqCh
+
+		overview, ok := req.KeyDiff.Content.(*KeyDiff_Overview)
+		if !ok {
+			resCh <- KeyDiffResult{Error: ErrKeyDiffRequestNotOverview}
+			return
+		}
+
+		inserts = overview.Overview.Inserts
+		updates = overview.Overview.Updates
+		deletions = overview.Overview.Deletions
+
+		for req := range reqCh {
+			changes, ok := req.KeyDiff.Content.(*KeyDiff_Changes)
+			if !ok {
+				resCh <- KeyDiffResult{Error: ErrKeyDiffRequestNotChanges}
+				return
+			}
+
+			for _, change := range changes.Changes.Changes {
+				switch change.Type {
+				case KeyDiffChangeType_INSERT:
+					diff.Inserts[change.Key] = string(change.Value)
+				case KeyDiffChangeType_UPDATE:
+					diff.Updates[change.Key] = string(change.Value)
+				case KeyDiffChangeType_DELETION:
+					diff.Deletions = append(diff.Deletions, change.Key)
+				default:
+					resCh <- KeyDiffResult{Error: ErrInvalidChangeType}
+					return
+				}
+			}
+		}
+
+		if int64(len(diff.Inserts)) != inserts || int64(len(diff.Updates)) != updates || int64(len(diff.Deletions)) != deletions {
+			resCh <- KeyDiffResult{Error: ErrInvalidChangeType}
+		}
+
+		resCh <- KeyDiffResult{KeyDiff: diff}
+	}()
+
+	return resCh
+}
 
 func getNewSendKeyDiffRequestCh() *SendKeyDiffRequest {
 	return &SendKeyDiffRequest{
